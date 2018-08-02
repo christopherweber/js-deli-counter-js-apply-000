6f31fcf76e8a39d0ded898f0cca178908763b159
Christopher Weber <chrisrrweber@gmail.com>
2018-08-02 18:44:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function nowServing(katzDeliLine) {
         var update = "The line is currently: ";
         deliLine.forEach(function(item, index){
         update += index+1 +"." + " " + item + "," + " "});
+        str = str.replace(/,\s*$/, "");
         } return update
   }
 
