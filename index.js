476e59f3d81acc0b103d125172fa3e5928ff8cb9
Christopher Weber <chrisrrweber@gmail.com>
2018-08-02 18:46:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
         var update = "The line is currently: ";
         deliLine.forEach(function(item, index){
         update += index+1 +"." + " " + item + "," + " "});
-        } return update, newUpdate
+        } return update
   }
 
  
